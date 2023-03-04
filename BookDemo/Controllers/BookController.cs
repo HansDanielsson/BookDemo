d015f8ea2ed1d8a4561fd26152f32b3a6818b768
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2023-03-04 17:07:25 +0100

    Add project files.

---
+++ BookDemo/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BookDemo.Controllers
+{
+  public class BookController : Controller
+  {
+    //
+    // GET /Book
+    public IActionResult Index()
+    {
+      return View();
+    }
+    //
+    // GET: /Book/Welcome
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+      ViewData["BookName"] = "Book name " + name;
+      ViewData["BookId"] =numTimes;
+      return View();
+    }
+  }
+}
---
+++ BookDemo/Data/BookDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookDemo.Models;
+
+namespace BookDemo.Data
+{
+    public class BookDemoContext : DbContext
+    {
+        public BookDemoContext (DbContextOptions<BookDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookDemo.Models.Book> Book { get; set; } = default!;
+    }
+}
